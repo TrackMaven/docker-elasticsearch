c4d511f368c49c381e43197aa30e7d43a44e215f
Cameron Maske <cameronmaske@gmail.com>
2015-12-09 14:43:25 -0500

    Fixed release script

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-docker build -t --no-cache quay.io/trackmaven/elasticsearch:1.7 1.7
+docker build --no-cache -t quay.io/trackmaven/elasticsearch:1.7 1.7
 docker push quay.io/trackmaven/elasticsearch:1.7
-docker build -t --no-cache quay.io/trackmaven/elasticsearch:2.1 2.1
+docker build --no-cache -t quay.io/trackmaven/elasticsearch:2.1 2.1
 docker push quay.io/trackmaven/elasticsearch:2.1
